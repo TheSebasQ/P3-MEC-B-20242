2452cb2a4cc12e1008002e5781a5dfe533199490
TheSebasQ <est.sebastianc.qui2@unimilitar.edu.co>
2024-08-27 15:22:48 -0500

    Create pg1.java

---
+++ P1/pg1.java
@@ -0,0 +1,11 @@
+package com.mycompany.mavenproject2;
+public class Mavenproject2 {
+//Crear una clase llamada Triangulo con atributos: base y altura.
+//Aplicar encapsulamiento y los metodos apropiados para inicializar o crear objetos con sus respectivos atributos.
+    public static void main(String[] args) {
+       class Triangulo {
+        private base;
+        protected altura; 
+    }
+    }
+}
