ef07410edd4c41a98781e83f7a6ce485f82dc34a
TheSebasQ <est.sebastianc.qui2@unimilitar.edu.co>
2024-08-27 15:43:24 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,24 @@
+package com.mycompany.mavenproject2;
+public class Mavenproject2 {
+//Crear 2 metodos puros que permitan obtener area y perimetro del triangulo, asumiendo qu todos son tringulos rectangulos 
+    public static void main(String[] args) {
+       class Triangulo { 
+          
+    }
+    }
+    
+    private class void Perimetro (){
+        int lado_a=23;
+        int lado_b=28;
+        int lado_c=32;
+        
+        int Perimetro= lado_a + lado_b + lado_c;
+}
+
+    private class void Area (){
+        int lado_a=23;
+        int lado_b=28;
+        int lado_c=32;
+        
+        int Area= lado_a
+    }
