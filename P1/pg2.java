a00ad2dc1d7c06c5c0ba4119f182311db39f88ef
TheSebasQ <est.sebastianc.qui2@unimilitar.edu.co>
2024-08-27 15:32:31 -0500

    Create pg2.java

---
+++ P1/pg2.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject2;
+public class Mavenproject2 {
+//A partir de la misma clase del punto anterior, crear sobrecarga al constructor y crear 2 objetos para cada una de las formas del constructor 
+    public static void main(String[] args) {
+       class Triangulo { 
+          
+    }
+    }
+}
